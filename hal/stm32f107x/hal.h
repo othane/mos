7bbe34d8cd81c08dabce1b2478b31dbebc47963b
Oliver Thane <othane@gmail.com>
2013-01-31 11:26:05 +1300

    add/tidy a hal interface
    I dont like this but am going to get going for the moment, hal interface
    should be at the root of hal, not implimentation specific. Ill sort this
    out later though.

---
+++ hal/stm32f107x/hal.h
@@ -0,0 +1,24 @@
+/**
+ * @file hal.h
+ * 
+ * @brief interface to the mos hal layer
+ *
+ * @author OT
+ *
+ * @date Jan 01 2013
+ *
+ */
+
+#ifndef __HAL__
+#define __HAL__
+
+/* hal dependancies */
+#include <stdint.h>
+#include <stm32f10x_conf.h>
+
+/* hal components */
+#include "sys.h"
+///@todo add other hal layers here as needed
+
+#endif
+
