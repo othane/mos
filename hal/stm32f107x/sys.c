fb50afaec22a8ee23dae72f08be8c42857dee2da
Oliver Thane <othane@gmail.com>
2013-01-31 11:10:48 +1300

    add a sys module for stm32f107
    Start fleshing out the hal sys module the stm32f107. Currently it will
    boot the blade and run the clk at 62.5MHz, and some simple ticks and critcal
    section stuff is running.

---
+++ hal/stm32f107x/sys.c
@@ -0,0 +1,206 @@
+/**
+ * @file sys.c
+ * 
+ * @brief implements the sys module of mos for the stm32f107x cpu
+ *
+ * @author OT
+ *
+ * @date Jan 01 2013
+ *
+ */
+
+
+#include "hal.h"
+
+
+/* internal structure used to store system states etc so they are all in 
+ * one easy place to find. */
+struct SYS_T
+{
+	volatile uint32_t ticks;
+	volatile int32_t critical_section_count;
+	enum SYS_ERR error;
+};
+static struct SYS_T sys = {0,};
+
+
+// setup all the system clocks
+#define SYS_CLK 72000000
+static void sys_clk_init(void)
+{
+	ErrorStatus HSEStartUpStatus;
+
+	// set clocks registers back to defaults (for debugging, st_demo)
+	RCC_DeInit();
+	
+	// enable the high speed external osc (HSE) and spin for it to stabilise
+	RCC_HSEConfig(RCC_HSE_ON);
+	HSEStartUpStatus = RCC_WaitForHSEStartUp();
+	if (HSEStartUpStatus != SUCCESS)
+	{
+		///@todo handle error better
+		while (1)
+		{}
+	}
+	
+	// Enable Prefetch Buffer
+	FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
+	
+	// Flash 1 wait state
+	FLASH_SetLatency(FLASH_Latency_2);
+	
+	// AHB prescaler set to div 1, HCLK = SYSCLK (72MHz)
+	RCC_HCLKConfig(RCC_SYSCLK_Div1); 
+
+	// APB2 (high speed) prescaler set to div 1, APB2 = HCLK
+	RCC_PCLK2Config(RCC_HCLK_Div1);
+	
+	// APB1 (low speed) prescaler set to div 2, APB1 = HCLK / 2
+	RCC_PCLK1Config(RCC_HCLK_Div2);
+
+	// Setup the PLLCLK source and pre scaler (25MHz * 5 / 2 = 62MHz)
+	///@note if you change this update the SYS_CLK definition
+	RCC_PREDIV1Config(RCC_PREDIV1_Source_HSE, RCC_PREDIV1_Div2);
+	RCC_PLLConfig(RCC_PLLSource_PREDIV1, RCC_PLLMul_5); 
+	
+	// Enable the PLL and spin for it to be ready
+	RCC_PLLCmd(ENABLE);
+	while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+        {}
+
+	// Switch the system clock over to the PLL output and spin until it is ready
+	RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
+	while(RCC_GetSYSCLKSource() != 0x08);
+
+#ifdef DEBUG_MCO
+	/* this is a handy debugging option to output the sys_clk/2 to the MCO p
+	 * it must be sys_clk/2 as gpio can only output 50MHz max, so 72MHz/2 = 36MHz */
+	RCC_MCOConfig(RCC_MCO_PLLCLK_Div2);
+#endif
+}
+
+
+// init system interrupts
+static void sys_interrupt_init(void)
+{
+	///@todo stub for now (we should at least call NVIC_PriorityGroupConfig() here
+}
+
+
+// init the on chip temperature sensor
+static void sys_temp_init(void)
+{
+	///@todo this is just a stub for now
+}
+
+
+// init a log via uart or trace
+static void sys_log_init(void)
+{
+	// for debugging via trace on jtag (very good !!)
+	DBGMCU->CR |= DBGMCU_CR_TRACE_IOEN;
+
+	///@todo uart log
+}
+
+
+// disable all interrupts and inc the counter so this can be re-entrant
+void sys_enter_critical_section(void)
+{
+	__disable_irq();
+	sys.critical_section_count++;
+
+	// error checking (have we rolled over or something
+	if (sys.critical_section_count < 0)
+	{
+		///@todo log error !
+		sys.critical_section_count = 1;
+	}
+}
+
+
+// decrement the critical section counter and if it hits 0 re-enable interrupts
+void sys_leave_critical_section(void)
+{
+	sys.critical_section_count--;
+	if (sys.critical_section_count <= 0)
+	{
+		__enable_irq();
+
+		// in case count get out of sync, log an error, and try to recover by resetting
+		if (sys.critical_section_count < 0)
+		{
+			///@todo log error !
+			sys.critical_section_count = 0;
+		}
+	}
+}
+
+
+// setup the tick handler interrupt rate
+static void sys_tick_init()
+{
+	if (SysTick_Config(SYS_CLK / 1000) != 0)
+	{}
+
+	NVIC_SetPriority(SysTick_IRQn, 2);
+}
+
+
+// sys tick ISR (overrides weak functions from st libs)
+void SysTick_Handler(void)
+{
+	sys.ticks++;
+}
+
+
+// get the current system time with ms resolution, use a shadow to protect against interrupts
+uint32_t sys_get_tick(void)
+{
+	uint32_t tick_shadow;
+
+	sys_enter_critical_section();
+	tick_shadow = sys.ticks;
+	sys_leave_critical_section();
+
+	return(tick_shadow);
+}
+
+
+// soft reset the whole cpu
+void sys_reset(void)
+{
+	NVIC_SystemReset();
+}
+
+
+///@todo need a prototype for printf
+void sys_log()
+{
+}
+
+
+// get the last error logged at the system level
+enum SYS_ERR sys_get_error(void)
+{
+	enum SYS_ERR sys_error_shadow;
+
+	sys_enter_critical_section();
+	sys_error_shadow = sys.error;
+	sys_leave_critical_section();
+
+	return sys_error_shadow;
+}
+
+
+// setup the basic components of any system
+void sys_init(void)
+{
+	sys_clk_init();
+	sys_interrupt_init();
+	sys_tick_init();
+	sys_temp_init();
+	sys_log_init();
+}
+
+
---
+++ hal/stm32f107x/sys.h
@@ -0,0 +1,78 @@
+/**
+ * @file sys.h
+ * 
+ * @brief interface to the system level functions
+ *
+ * @author OT
+ *
+ * @date Jan 01 2013
+ *
+ */
+
+#ifndef __SYS__
+#define __SYS__
+
+
+#define SYS STM32f107
+
+
+/**
+ * @brief simple set of system wide error codes
+ */
+enum SYS_ERR
+{
+	SYS_ERR_NONE = 0, /**< no pending system errors */
+};
+
+
+/**
+ * @brief enter a critical section that cannot be interrupted by any other process or isq
+ * @see sys_end_critical_section
+ * @note this should be re-entrant, ie if called twice in a row, it would take 2 corresponding calls to sys_leave_critical_section to allow processes/isrs to run again
+ */
+void sys_enter_critical_section(void);
+
+
+/**
+ * @brief leave a critical section, allow other processes/isq
+ * @see sys_start_critical_section
+ * @note this should be re-entrant, ie if sys_enter_critical_section is called twice in a row, it would take 2 corresponding calls to allow processes/isrs to run again
+ */
+void sys_leave_critical_section(void);
+
+
+/**
+ * @brief get the number of 1ms intervals since boot
+ * @note there is not attempt to deal with rollovers in this function
+ * @return the number of 1ms ticks that have occurred since boot time
+ */
+uint32_t sys_get_tick(void);
+
+
+/**
+ * @brief do a software reset of the system
+ */
+void sys_reset(void);
+
+
+/**
+ * @todo need a prototype for this
+ */
+void sys_log();
+
+
+/**
+ * @brief get the last error code logged by the system
+ * @return error code indicating the last problem seen by the system
+ */
+enum SYS_ERR sys_get_error(void);
+
+
+/**
+ * @brief Initialise the hal system level
+ * @note call this first thing on start-up
+ */
+void sys_init(void);
+
+#endif
+
