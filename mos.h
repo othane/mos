1218335317b2190702887b9ba6329c5f5f9575a7
Oliver Thane <othane@gmail.com>
2013-02-05 17:10:01 +1300

    add main interface to mos
    only has links to hal atm as this is all that has been written

---
+++ mos.h
@@ -0,0 +1,18 @@
+/**
+ * @file mos.h
+ * 
+ * @brief interface to the micro os modules (hal, containers, etc)
+ *
+ * @author OT
+ *
+ * @date Jan 2013
+ *
+ */
+
+#ifndef __MOS__
+#define __MOS__
+
+#include "hal/stm32f107x/hal.h"
+
+#endif
+
