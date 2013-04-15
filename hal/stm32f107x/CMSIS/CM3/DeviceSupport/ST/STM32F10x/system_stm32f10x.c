25133a4eb954deb38d17265eb1f1943e8a7410e0
Oliver Thane <othane@gmail.com>
2013-04-15 12:29:32 +1200

    stop startup code from changing boot mode to RAM
    SystemInit which is called in the startup scripts was setting the
    SCB->VTOR to boot from flash, but we change this to boot from RAM in the
    bootstrap so we MUST modify this this (this is not ideal as it is from
    the CMSIS libraries and hence we would need to apply this patch after
    each update)

--- hal/stm32f107x/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.c
+++ hal/stm32f107x/CMSIS/CM3/DeviceSupport/ST/STM32F10x/system_stm32f10x.c
@@ -261,11 +261,13 @@ void SystemInit (void)
   /* Configure the Flash Latency cycles and enable prefetch buffer */
   SetSysClock();
 
+#ifdef SET_VECT_TAB_SRC
 #ifdef VECT_TAB_SRAM
   SCB->VTOR = SRAM_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal SRAM. */
 #else
   SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal FLASH. */
 #endif 
+#endif
 }
 
 /**
