68efbada5f52ae947b3ba3f0cebb1537aea09ca8
Oliver Thane <othane@gmail.com>
2013-04-16 18:06:35 +1200

    add crc unit test
    this tests a msg with a known crc and checks all methods (soft, table
    and hard) agree the value is as known
    
    @todo this only tests the 32bit stm32f10x series crc method, we should
    at least check a 8bit soft and table version as this is usefull for
    things like serial comms

--- utest/crc/crc_utest.c
+++ utest/crc/crc_utest.c
@@ -12,6 +12,11 @@
  *
  */
 
+#include <crc.h>
+#ifdef EMBEDDED 
+#include <hal.h>
+#endif
+
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -19,49 +24,44 @@
 #include <stdio.h>
 #endif
 
-#ifdef CRC_HARD
-#include <hal.h>
-#else
-#include "crc.h"
-#endif
-
 
 static const char msg[] = "Hello world this is a message to crc";
-static uint32_t msg_known_crc = 0xDF750117;
+static uint32_t msg_known_crc = 0xb3c8cbe8;
+uint32_t crc_tbl[256];
+struct crc_h h =
+{
+	{32, 0x04C11DB7, 0xFFFFFFFF, FALSE, FALSE, 0, 0},
+	crc_tbl,
+	sizeof(crc_tbl),
+	CRC_METHOD_BEST,
+};
+bool result = false;
 
 
 void init(void)
 {
-	#ifdef CRC_HARD
+	#ifdef EMBEDDED
 	sys_init();
 	#endif
+
 }
 
 
-#ifdef CRC_SOFT
-uint32_t _crc_soft(void *buf, uint32_t len);
-#endif
-#ifdef CRC_HARD
-uint32_t _crc_hard(void *buf, uint32_t len);
-#endif
-
-
-bool result = false; // assume test fails and prove it passes
-void main(void)
+uint32_t run_crc(void)
 {
 	uint32_t crc;
-	#ifdef CRC_SOFT
-	uint32_t crc_soft;
-	#endif
-	#ifdef CRC_HARD
-	uint32_t crc_hard;
-	#endif
+	uint32_t len;
 
-	init();
+	// mangle the message length to be 4 byte aligned so we can compare HW with other methods
+	len = sizeof(msg);
+	if (len & 0x03)
+		len = (len & 0xFFFFFFFC) + 0x04;
+	else
+		len = (len & 0xFFFFFFFC);
 
-	// crc msg
-	crc = crc32((void *)msg, sizeof(msg));
-
+	crc_init(&h);
+	crc = crc_buf(&h, msg, len);
+	
 	// check unit test results
 	if (crc == msg_known_crc)
 		result = true;
@@ -72,28 +72,30 @@ void main(void)
 		printf("test failed:\n");
 	printf("crc = 0x%.4X%\n", crc);
 	#endif
-
-	#ifdef CRC_SOFT
-	// crc msg via software
-	crc_soft = _crc_soft((void *)msg, sizeof(msg));
-	#ifdef PRINT_RESULT
-	printf("crc_soft = 0x%.4X%\n", crc_soft);
-	#endif
-	#endif
 	
-	#ifdef CRC_HARD
-	// crc msg via hardware
-	crc_hard = _crc_hard((void *)msg, sizeof(msg));
-	#ifdef PRINT_RESULT
-	printf("crc_hard = 0x%.4X%\n", crc_hard);
-	#endif
-	#endif
+	return crc;
+}
 
-	#ifndef PRINT_RESULT
+
+int main(void)
+{
+	uint32_t crc_soft, crc_tab, crc_hard;
+
+	init();
+	
+	h.method = CRC_METHOD_SOFT;
+	crc_soft = run_crc();
+	h.method = CRC_METHOD_TABLE_32W;
+	crc_tab  = run_crc();
+	h.method = CRC_METHOD_BEST;
+	crc_hard = run_crc();
+
+	#ifdef EMBEDDED 
 	// done	
 	while (1)
-	{
-	}
+	{}
 	#endif
+
+	return 0;
 }
 
