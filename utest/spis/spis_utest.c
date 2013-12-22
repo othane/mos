7e34981234ca236bf2796d4699e042e88840f516
Oliver Thane <othane@gmail.com>
2013-12-23 10:33:09 +1300

    simplify spis utest & add stm32f373 support
    clean up and simplify the spis unit test so there is only 1 simple test
    to pass/fail (atm I use a raspberry pi as the master, see
    http://www.100randomtasks.com/simple-spi-on-raspberry-pi)
    
    add stm32f373 to the hw support
    
    ensure hal is built on every make call

--- utest/spis/spis_utest.c
+++ utest/spis/spis_utest.c
@@ -16,6 +16,37 @@
 #include <hal.h>
 
 
+#define WRITE_BUF_LEN 8
+#define READ_BUF_LEN 8
+static uint8_t write_buf[WRITE_BUF_LEN] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07};
+static uint8_t read_buf[READ_BUF_LEN] = {0,};
+uint16_t wcount = 0;
+uint16_t rcount = 0;
+
+
+void write_complete(spis_t *spis, void *buf, uint16_t len, void *param)
+{
+	wcount++;
+}
+
+
+void read_complete(spis_t *spis, void *buf, uint16_t len, void *param)
+{
+	rcount++;
+
+	// echo back
+	memcpy(write_buf, buf, len);
+	spis_write(spis, buf, len, write_complete, NULL);
+}
+
+void test_init(void)
+{
+	// kick off io
+	spis_write(&spis3, write_buf, WRITE_BUF_LEN, write_complete, NULL);
+	spis_read(&spis3, read_buf, READ_BUF_LEN, read_complete, NULL);
+}
+
+
 void spis3_select(spis_t *spis, void *param)
 {
 	sys_nop();
@@ -24,7 +55,9 @@ void spis3_select(spis_t *spis, void *param)
 
 void spis3_deselect(spis_t *spis, void *param)
 {
-	sys_nop();
+	spis_cancel_read(spis);
+	spis_cancel_write(spis);
+	test_init();
 }
 
 
@@ -37,96 +70,7 @@ void init(void)
 }
 
 
-#define WRITE_BUF_LEN 6
-#define READ_BUF_LEN 3
-static uint8_t write_buf[WRITE_BUF_LEN] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05};
-static uint8_t read_buf[6] = {0,};
-uint16_t wcount = 0;
-uint16_t rcount = 0;
-
-#define TEST1
-void read_complete(spis_t *spis, void *buf, uint16_t len, void *param);
-
-#ifdef TEST1
-void write_complete(spis_t *spis, void *buf, uint16_t len, void *param)
-{
-	int k;
-	wcount++;
-	///@todo for some reason we need this delay or the io goes bad ?!?!?
-	for (k = 500; k > 0; k--)
-	{}
-	// keep reading
-	spis_write(&spis3, write_buf, WRITE_BUF_LEN, write_complete, NULL);
-	spis_read(&spis3, read_buf, READ_BUF_LEN, read_complete, NULL);
-}
-
-
-void read_complete(spis_t *spis, void *buf, uint16_t len, void *param)
-{
-	rcount++;
-	if (((uint8_t *)buf)[1] != 0x01)
-		sys_nop();
-
-	// echo back
-	memcpy(&write_buf[READ_BUF_LEN], buf, len);
-}
-
-void test_init(void)
-{
-	// kick off io
-	spis_write(&spis3, write_buf, WRITE_BUF_LEN, write_complete, NULL);
-	spis_read(&spis3, read_buf, READ_BUF_LEN, read_complete, NULL);
-}
-#endif
-
-
-#ifdef TEST2
-void test_init(void)
-{
-	// read the first 3 bytes
-	spis_read(&spis3, read_buf, READ_BUF_LEN, read_complete, NULL);
-}
-
-void read_complete2(spis_t *spis, void *buf, uint16_t len, void *param)
-{
-	rcount++;
-	// read the first 3 bytes
-	spis_read(&spis3, read_buf, READ_BUF_LEN, read_complete, NULL);
-}
-
-void read_complete(spis_t *spis, void *buf, uint16_t len, void *param)
-{
-	rcount++;
-	// read remaining 3 bytes
-	spis_read(&spis3, &read_buf[READ_BUF_LEN], READ_BUF_LEN, read_complete2, NULL);
-}
-#endif
-
-#ifdef TEST3
-void write_complete(spis_t *spis, void *buf, uint16_t len, void *param);
-void test_init(void)
-{
-	// write half of the fixed pattern
-	spis_write(&spis3, write_buf, 3, write_complete, NULL);
-}
-
-void write_complete2(spis_t *spis, void *buf, uint16_t len, void *param)
-{
-	rcount++;
-	// write half of the fixed pattern
-	spis_write(&spis3, write_buf, 3, write_complete, NULL);
-}
-
-void write_complete(spis_t *spis, void *buf, uint16_t len, void *param)
-{
-	rcount++;
-	// write the rest of the fixed pattern
-	spis_write(&spis3, &write_buf[3], 3, write_complete2, NULL);
-}
-#endif
-
-
-void main(void)
+int main(void)
 {
 	init();
 	test_init();
@@ -135,5 +79,7 @@ void main(void)
 	while (1)
 	{
 	}
+
+	return 0;
 }
 
