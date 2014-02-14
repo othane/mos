4b8ae67a1425cec3223f9e3d92f2e35625706317
Oliver Thane <othane@gmail.com>
2014-02-14 17:12:39 +1300

    ops, fix bootstrap isr vectors on the stm32f373
    the new system init was restoring the VTOR register on startup of the
    stm32f373 meaning that it was using the ISR's from flash not ram (as
    changed by the bootstrap) ... this patch fixes that

--- hal/stm32f373/STM32F37x_DSP_StdPeriph_Lib_V1.0.0/Libraries/CMSIS/Device/ST/STM32F37x/Source/Templates/system_stm32f37x.c
+++ hal/stm32f373/STM32F37x_DSP_StdPeriph_Lib_V1.0.0/Libraries/CMSIS/Device/ST/STM32F37x/Source/Templates/system_stm32f37x.c
@@ -201,10 +201,12 @@ void SystemInit (void)
   SetSysClock();
 
   
+#ifdef SET_VECT_TAB_SRC
 #ifdef VECT_TAB_SRAM
   SCB->VTOR = SRAM_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal SRAM. */
 #else
   SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal FLASH. */
+#endif
 #endif
 }
 
