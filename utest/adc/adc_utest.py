cb97087b9f855be62cee9236303a4cb72d489306
Oliver Thane <othane@gmail.com>
2015-04-22 10:28:22 +1200

    tidy-up adc unit test (py)

--- utest/adc/adc_utest.py
+++ utest/adc/adc_utest.py
@@ -4,34 +4,6 @@ import matplotlib.animation as animation
 import subprocess
 import os
 
-def genADCSamples(filename, count=4096):
-    cmd = [
-        # start gdb and connect
-        "arm-none-eabi-gdb",
-        #"-x \"debug_adc_utest.gdbinit\"",
-        "-x \"run_adc_utest.gdbinit\"",
-        # dump adc sample buffer to filename
-        "-ex \"dump binary memory %s &buf[0] (&buf[%d])\"" % (filename, count),
-        # quit
-        "-ex \"quit\"",
-    ]
-    subprocess.call(' '.join(cmd), shell=True)
-
-def loadADCSamples(filename, fmt="H"):
-    buf = open(filename).read()
-    size = struct.Struct(fmt).size
-    u = struct.unpack("<%d" % (len(buf)/size) + fmt, buf)
-    return array(list(u))
-
-def showADCSamples(buf, scale=3.3/65536):
-    fs = 6e6 / 120
-    ts = 1 / fs
-    t = arange(0, (len(buf))*ts, ts)
-    plot(t, buf * scale)
-    grid(True)
-    ylim([0, 3.5])
-    show()
-
 class scope():
 
     def __init__(self):
@@ -45,10 +17,25 @@ class scope():
         self.traces, = self.ax.plot([], [], '-b')
         self.anim = animation.FuncAnimation(self.fig, self.animate, interval=1e1, repeat=True, blit=True)
 
+    def getADCSamples(self, count=4096, fmt="H"):
+        cmd = [
+            # start gdb and connect
+            "arm-none-eabi-gdb",
+            #"-x \"debug_adc_utest.gdbinit\"",
+            "-x \"run_adc_utest.gdbinit\"",
+            # dump adc sample buffer to filename
+            "-ex \"dump binary memory %s &buf[0] (&buf[%d])\"" % (self.filename, count),
+            # quit
+            "-ex \"quit\"",
+        ]
+        subprocess.call(' '.join(cmd), shell=True)
+        buf = open(self.filename).read()
+        size = struct.Struct(fmt).size
+        u = struct.unpack("<%d" % (len(buf)/size) + fmt, buf)
+        return array(list(u))
+
     def animate(self, n):
-        genADCSamples(self.filename)
-        samples = loadADCSamples(self.filename)
-        #samples = rand(4096)
+        samples = self.getADCSamples()
         t = arange(0, (len(samples))*self.ts, self.ts)
         self.traces.set_data(t, samples)
         self.fig.canvas.draw()
